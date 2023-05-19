8f75aa39a21ba9eaa9fac843259316eae839fa69
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2023-05-19 15:47:18 -0300

    HomeReadyForAnimation

--- 01-Home/script.js
+++ 01-Home/script.js
@@ -7,49 +7,50 @@ function home(){
     
     const label = document.createElement("div")
     div.innerHTML =
-    `<div class="home centralize">
-        <div class="title">
-            Avaliação do primeiro semestre de Análise e Desenvolvimento de Sistemas
+        `
+        <div class="home centralize">
+            <div class="title">
+                Avaliação do primeiro semestre de Análise e Desenvolvimento de Sistemas
+            </div>
+            <div class="subTitle">
+                ESCOLHA A MATÉRIA
+            </div>
         </div>
-        <div class="subTitle">
-            ESCOLHA A MATÉRIA
-        </div>
-    </div>
 
-    <div class="scrollMe">
-        <div class="cards">
-            <a href="../ProgramLanguage/programlanguage.html">
-                <div class="card">
-                    <p class="tip">Linguagem de Programação</p>
-                    <p class="second-text">Lorem Ipsum</p>
-                </div>
-            </a>
-            <a href="../DataBase/database.html">
-                <div class="card">
-                    <p class="tip">Banco de Dados</p>
-                    <p class="second-text">Lorem Ipsum</p>
-                </div>
-            </a>
-            <a href="../Network/network.html">
-                <div class="card">
-                    <p class="tip">Redes de Computadores</p>
-                    <p class="second-text">Lorem Ipsum</p>
-                </div>
-            </a>
-            <a href="../DataScience/datascience.html">
-                <div class="card">
-                    <p class="tip">Ciência de Dados</p>
-                    <p class="second-text">Lorem Ipsum</p>
-                </div>
-            </a>
-            <a href="../Automation/automation.html">
-                <div class="card">
-                    <p class="tip">Automação Industrial</p>
-                    <p class="second-text">Lorem Ipsum</p>
-                </div>
-            </a>
+        <div class="chooseMe">
+            <div class="cards">
+                <a href="../ProgramLanguage/programlanguage.html">
+                    <div class="card">
+                        <p class="tip">Linguagem de Programação</p>
+                        <p class="second-text">Java</p>
+                    </div>
+                </a>
+                <a href="../DataBase/database.html">
+                    <div class="card">
+                        <p class="tip">Banco de Dados</p>
+                        <p class="second-text">Postgree · DER · MER</p>
+                    </div>
+                </a>
+                <a href="../Network/network.html">
+                    <div class="card">
+                        <p class="tip">Redes de Computadores</p>
+                        <p class="second-text">IP · DNS · TCP</p>
+                    </div>
+                </a>
+                <a href="../DataScience/datascience.html">
+                    <div class="card">
+                        <p class="tip">Ciência de Dados</p>
+                        <p class="second-text">PYTHON</p>
+                    </div>
+                </a>
+                <a href="../Automation/automation.html">
+                    <div class="card">
+                        <p class="tip">Automação Industrial</p>
+                        <p class="second-text">C++ · Arduino · Eletrônica</p>
+                    </div>
+                </a>
+            </div>
         </div>
-    </div>
-    `
+        `
     document.body.appendChild(div);
 }
