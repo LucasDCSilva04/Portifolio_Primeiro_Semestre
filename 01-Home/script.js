cc385bd58a40b544414a1d7eedc915b2e2a3869c
LucasDCSilva04 <jeffersondlsilva@hotmail.com>
2023-05-20 01:01:51 -0300

    HomeWithAnimation

--- 01-Home/script.js
+++ 01-Home/script.js
@@ -20,33 +20,43 @@ function home(){
         <div class="choose">
             <div class="cards">
                 <a class="go" onclick=hideAll()>
-                    <div class="card">
-                        <p class="tip">Linguagem de Programação</p>
-                        <p class="second-text">Java</p>
+                    <div class="move1">
+                        <div class="card" id="card1">
+                            <p class="tip">Linguagem de Programação</p>
+                            <p class="second-text">Java</p>
+                        </div>
+                    </div>    
+                </a>
+                <a class="go" onclick=hideAll()>
+                    <div class="move2">
+                        <div class="card" id="card2">
+                            <p class="tip">Banco de Dados</p>
+                            <p class="second-text">Postgree · DER · MER</p>
+                        </div>
                     </div>
                 </a>
                 <a class="go" onclick=hideAll()>
-                    <div class="card">
-                        <p class="tip">Banco de Dados</p>
-                        <p class="second-text">Postgree · DER · MER</p>
+                    <div class="move3">
+                        <div class="card" id="card3">
+                            <p class="tip">Redes de Computadores</p>
+                            <p class="second-text">IP · DNS · TCP</p>
+                        </div>
+                    </div>    
+                </a>
+                <a class="go" onclick=hideAll()>
+                    <div class="move4">
+                        <div class="card" id="card4">
+                            <p class="tip">Ciência de Dados</p>
+                            <p class="second-text">PYTHON</p>
+                        </div>
                     </div>
                 </a>
                 <a class="go" onclick=hideAll()>
-                    <div class="card">
-                        <p class="tip">Redes de Computadores</p>
-                        <p class="second-text">IP · DNS · TCP</p>
-                    </div>
-                </a>
-                <a class="go" onclick=hideAll()>
-                    <div class="card">
-                        <p class="tip">Ciência de Dados</p>
-                        <p class="second-text">PYTHON</p>
-                    </div>
-                </a>
-                <a class="go" onclick=hideAll()>
-                    <div class="card">
-                        <p class="tip">Automação Industrial</p>
-                        <p class="second-text">C++ · Arduino · Eletrônica</p>
+                    <div class="move5">
+                        <div class="card" id="card5">
+                            <p class="tip">Automação Industrial</p>
+                            <p class="second-text">C++ · Arduino · Eletrônica</p>
+                        </div>
                     </div>
                 </a>
             </div>
@@ -63,46 +73,51 @@ function homeFadeIn(){
     document.querySelector(".home").style.opacity=1;
 }
 
+var i = 1;
+
 function cardIn(){
-    document.querySelector(".cards").style.opacity=1; 
-}
+        setTimeout(function() {
+            document.querySelector(`#card${i}`).style.opacity=1;
+            document.querySelector(`.move${i}`).style.transform="translateX(-100px)";
+            incrementa();
+        }, 300);
+    }
 
-function homeFadeOut(){
-    document.querySelector(".home").style.opacity=0;
-}
+function incrementa(){
+    if(i<5)
+        i++;
 
-function cardOut(){
-    document.querySelector(".cards").style.opacity=0;
+    cardIn();
 }
 
 function hideAll(){
-    homeFadeOut();
-    cardOut();
+    document.querySelector(".cards").style.opacity=0;
+    document.querySelector(".home").style.opacity=0;
 }
 
 function goLP(){
     setTimeout(() => {
         window.location.href = "../01-Home/home.html";
-    }, 400);   
+    }, 600);   
 }
 
 function goBD(){
     setTimeout(() => {
         window.location.href = "../01-Home/home.html";
-    }, 400);}
+    }, 600);}
 
 function goRedes(){
     setTimeout(() => {
         window.location.href = "../01-Home/home.html";
-    }, 400);}
+    }, 600);}
 
 function goDataS(){
     setTimeout(() => {
         window.location.href = "../01-Home/home.html";
-    }, 400);
+    }, 600);
 }
 
 function goAuto(){
     setTimeout(() => {
         window.location.href = "../01-Home/home.html";
-    }, 400);}
+    }, 600);}
