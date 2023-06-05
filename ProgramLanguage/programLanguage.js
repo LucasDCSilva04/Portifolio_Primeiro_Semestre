7f4bdee61a1d62c91559366199de89d7ea7d82fc
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2023-06-05 17:00:46 -0300

    JSslides

--- ProgramLanguage/programLanguage.js
+++ ProgramLanguage/programLanguage.js
@@ -43,6 +43,7 @@ function createButton(){
 }
 
 var j=0;
+var max1=9;
 
 function back(){
     if(j>0){
@@ -64,14 +65,14 @@ function back(){
 }
 
 function next(){
-    if(j<9){
+    if(j<max1){
         document.querySelector(`.s${j}`).style.opacity=0;
         document.querySelector(`.s${j}`).style.transform="translateX(-100vw)";
         document.querySelector(`.s${j+1}`).style.opacity=1;
         document.querySelector(`.s${j+1}`).style.transform="translateX(0vw)";
         j++;
         control();
-    }else if(j==9){
+    }else if(j==max1){
         document.querySelector(`.s${j}`).style.transform="translateX(2vw)"
         setTimeout(() => {
             document.querySelector(`.s${j}`).style.transform="translateX(-3vw)"
@@ -91,17 +92,18 @@ function control(){
         document.querySelector(".btnBack").style.backgroundColor = "#0077ff";
         document.querySelector(".btnBack").style.opacity=1;
     }
-    if(j==9){
+    if(j==max1){
         document.querySelector(".btnNext").style.backgroundColor = "grey";
         document.querySelector(".btnNext").style.opacity=0.2;
     }
-    if(j<9){
+    if(j<max1){
         document.querySelector(".btnNext").style.backgroundColor = "#0077ff";
         document.querySelector(".btnNext").style.opacity=1;
     }
 }
 
 var k=0;
+var max2 = 15;
 
 function back2(){
     if(k>0){
@@ -123,14 +125,14 @@ function back2(){
 }
 
 function next2(){
-    if(k<9){
+    if(k<max2){
         document.querySelector(`.c${k}`).style.opacity=0;
         document.querySelector(`.c${k}`).style.transform="translateX(-100vw)";
         document.querySelector(`.c${k+1}`).style.opacity=1;
         document.querySelector(`.c${k+1}`).style.transform="translateX(0vw)";
         k++;
         control2();
-    }else if(k==9){
+    }else if(k==max2){
         document.querySelector(`.c${k}`).style.transform="translateX(2vw)"
         setTimeout(() => {
             document.querySelector(`.c${k}`).style.transform="translateX(-3vw)"
@@ -150,11 +152,11 @@ function control2(){
         document.querySelector(".btnBack2").style.backgroundColor = "#0077ff";
         document.querySelector(".btnBack2").style.opacity=1;
     }
-    if(k==9){
+    if(k==max2){
         document.querySelector(".btnNext2").style.backgroundColor = "grey";
         document.querySelector(".btnNext2").style.opacity=0.2;
     }
-    if(k<9){
+    if(k<max2){
         document.querySelector(".btnNext2").style.backgroundColor = "#0077ff";
         document.querySelector(".btnNext2").style.opacity=1;
     }
