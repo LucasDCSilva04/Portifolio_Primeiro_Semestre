cca3ffbb9277a7205f788d38e7996652b13f8aa3
LucasDCSilva04 <jeffersondlsilva@hotmail.com>
2023-06-09 12:37:39 -0300

    CSSoPTIMIZED

--- DataBase/database.js
+++ DataBase/database.js
@@ -40,6 +40,66 @@ function createButton(){
     document.querySelector("#btnHome").addEventListener("click", goHome);
 }
 
+var j=0;
+var max1=9;
+
+function back(){
+    if(j>0){
+        document.querySelector(`.s${j}`).style.opacity=0;
+        document.querySelector(`.s${j}`).style.transform="translateX(+100vw)";
+        document.querySelector(`.s${j-1}`).style.opacity=1;
+        document.querySelector(`.s${j-1}`).style.transform="translateX(0vw)";
+        j--;
+        control();
+    }else if(j==0){
+        document.querySelector(`.s${j}`).style.transform="translateX(-2vw)"
+        setTimeout(() => {
+            document.querySelector(`.s${j}`).style.transform="translateX(3vw)"
+            setTimeout(() => {
+                document.querySelector(`.s${j}`).style.transform="translateX(0vw)"
+            }, 200);
+        }, 200);   
+    }
+}
+
+function next(){
+    if(j<max1){
+        document.querySelector(`.s${j}`).style.opacity=0;
+        document.querySelector(`.s${j}`).style.transform="translateX(-100vw)";
+        document.querySelector(`.s${j+1}`).style.opacity=1;
+        document.querySelector(`.s${j+1}`).style.transform="translateX(0vw)";
+        j++;
+        control();
+    }else if(j==max1){
+        document.querySelector(`.s${j}`).style.transform="translateX(2vw)"
+        setTimeout(() => {
+            document.querySelector(`.s${j}`).style.transform="translateX(-3vw)"
+            setTimeout(() => {
+                document.querySelector(`.s${j}`).style.transform="translateX(0vw)"
+            }, 200);
+        }, 200);
+    }
+}
+
+function control(){
+    if(j==0){
+        document.querySelector(".btnBack").style.backgroundColor = "grey";
+        document.querySelector(".btnBack").style.opacity=0.2;
+    }
+    if(j>0){
+        document.querySelector(".btnBack").style.backgroundColor = "#0077ff";
+        document.querySelector(".btnBack").style.opacity=1;
+    }
+    if(j==max1){
+        document.querySelector(".btnNext").style.backgroundColor = "grey";
+        document.querySelector(".btnNext").style.opacity=0.2;
+    }
+    if(j<max1){
+        document.querySelector(".btnNext").style.backgroundColor = "#0077ff";
+        document.querySelector(".btnNext").style.opacity=1;
+    }
+}
+
 function goHome(){
     document.querySelector(".title").style.opacity=0;
     document.querySelector(".content").style.opacity=0;
