f7435c06678b8ce7514e2ab765909089d3c53da3
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2023-06-13 15:41:56 -0300

    medidasDispersao

--- DataScience/datascience.js
+++ DataScience/datascience.js
@@ -100,6 +100,66 @@ function control(){
     }
 }
 
+var k=0;
+var max2 = 2;
+
+function back2(){
+    if(k>0){
+        document.querySelector(`.c${k}`).style.opacity=0;
+        document.querySelector(`.c${k}`).style.transform="translateX(+100vw)";
+        document.querySelector(`.c${k-1}`).style.opacity=1;
+        document.querySelector(`.c${k-1}`).style.transform="translateX(0vw)";
+        k--;
+        control2();
+    }else if(k==0){
+        document.querySelector(`.c${k}`).style.transform="translateX(-2vw)"
+        setTimeout(() => {
+            document.querySelector(`.c${k}`).style.transform="translateX(3vw)"
+            setTimeout(() => {
+                document.querySelector(`.c${k}`).style.transform="translateX(0vw)"
+            }, 200);
+        }, 200);
+    }
+}
+
+function next2(){
+    if(k<max2){
+        document.querySelector(`.c${k}`).style.opacity=0;
+        document.querySelector(`.c${k}`).style.transform="translateX(-100vw)";
+        document.querySelector(`.c${k+1}`).style.opacity=1;
+        document.querySelector(`.c${k+1}`).style.transform="translateX(0vw)";
+        k++;
+        control2();
+    }else if(k==max2){
+        document.querySelector(`.c${k}`).style.transform="translateX(2vw)"
+        setTimeout(() => {
+            document.querySelector(`.c${k}`).style.transform="translateX(-3vw)"
+            setTimeout(() => {
+                document.querySelector(`.c${k}`).style.transform="translateX(0vw)"
+            }, 200);
+        }, 200);
+    }
+}
+
+function control2(){
+    if(k==0){
+        document.querySelector(".btnBack2").style.backgroundColor = "grey";
+        document.querySelector(".btnBack2").style.opacity=0.2;
+    }
+    if(k>0){
+        document.querySelector(".btnBack2").style.backgroundColor = "#0077ff";
+        document.querySelector(".btnBack2").style.opacity=1;
+    }
+    if(k==max2){
+        document.querySelector(".btnNext2").style.backgroundColor = "grey";
+        document.querySelector(".btnNext2").style.opacity=0.2;
+    }
+    if(k<max2){
+        document.querySelector(".btnNext2").style.backgroundColor = "#0077ff";
+        document.querySelector(".btnNext2").style.opacity=1;
+    }
+}
+
 function goHome(){
     document.querySelector(".title").style.opacity=0;
     document.querySelector(".content").style.opacity=0;
